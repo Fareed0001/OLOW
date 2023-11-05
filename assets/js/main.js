5f9a5d5e705457e97080b8c4513fe6dbabf94456
Farid <fareedibrahim0003@gmail.com>
2023-11-05 14:17:43 +0100

    completed code

--- assets/js/main.js
+++ assets/js/main.js
@@ -254,10 +254,49 @@
       mirror: false
     })
   });
-
-  /**
-   * Initiate Pure Counter 
-   */
-  new PureCounter();
-
 })()
+
+// CAROUSEL 
+document.addEventListener('DOMContentLoaded', () => {
+  
+  //===== MICRO-SLIDER begin
+	const __ms = document.querySelector('.micro-slider');
+  const __msSlider = new MicroSlider(__ms, { indicators: true, indicatorText: '' });
+  const hammer = new Hammer(__ms);
+	const __msTimer = 2000;
+  let __msAutoplay = setInterval(() => __msSlider.next(), __msTimer);
+    
+  //detect mouseenter event
+	__ms.onmouseenter = function(e) {
+    clearInterval(__msAutoplay); 
+    console.log(e.type + ' mouse detected');
+  }
+  
+  //detect mouseleave event
+	__ms.onmouseleave = function(e) {
+    clearInterval(__msAutoplay); 
+    __msAutoplay = setInterval(() => __msSlider.next(), __msTimer);
+    console.log(e.type + ' mouse detected');
+  }
+  
+  //detect mouseclick event
+	__ms.onclick = function(e) {
+    clearInterval(__msAutoplay); 
+    console.log(e.type + ' mouse detected');
+  }
+  
+  //detect gesture tap event with hammer js library
+  hammer.on('tap', function(e) {
+    clearInterval(__msAutoplay);
+    console.log(e.type + ' gesture detected');
+  });
+  
+  //detect gesture swipe event with hammer js library
+  hammer.on('swipe', function(e) {
+    clearInterval(__msAutoplay); 
+    __msAutoplay = setInterval(() => __msSlider.next(), __msTimer);
+    console.log(e.type + ' gesture detected');
+  });  
+  //===== MICRO-SLIDER end
+  
+});
